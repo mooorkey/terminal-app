bf7894b7dbdb88feede998d25b6901b925c69f9d
natkat9 <42442614+natkat9@users.noreply.github.com>
2019-04-23 14:18:53 +1000

    made blueprint of main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,41 @@
 require_relative './classes/hotel'
 require_relative './classes/room'
 require_relative './classes/cat'
-require_relative './classes/booking
+require_relative './classes/booking'
 
-# 
+# Welcome message
+puts "Meow! Welcome to the Purr Seasons Cat Hotel"
+
+# Enter Cat guest details
+
+# Display hotel menu options
+    # 1. Make a new booking
+    # 2. View an existing booking
+    # 3. View hotel room types
+    # 4. View hotel information
+    # 5. Q to Quit App
+
+# Make a new booking
+    # Display a list of room types
+    # Choose a room type
+    # Display details of room selected
+    # Ask user if they want to book this room or view another
+    # If they want to book, display availability, and get user selection
+    # Create a new booking with room type and booking days
+    # Display booking for user including price
+    # Option to return to main menu or quit
+
+# View an existing booking
+    # Dispaly booking for user
+    # Stretch: provide option to cancel
+    # Return to main menu or quit
+
+# View hotel room types
+    # Display a list of room types
+    # Choose a room type
+    # Display details of room selected
+    # Return to main menu or quit
+
+# View hotel information
+    # Display hotel details
+    # Return to main menu or quit
